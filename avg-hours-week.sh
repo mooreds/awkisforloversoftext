1f74edcf593b714aec0fd5371bea4a1c458aac17
Dan Moore <bitbucket@mooreds.com>
2018-10-18 06:41:17 -0600

    break script into sep lines for readability

--- avg-hours-week.sh
+++ avg-hours-week.sh
@@ -1,2 +1,6 @@
-awk -F, 'BEGIN { sum = 0; count = 0} $4 ~ /HS-grad/ {sum += $13; count++;} END {print "hs grad avg hrs/week:", sum/count}' data/adult.data 
-awk -F, 'BEGIN { sum = 0; count = 0} $4 ~ /Bach/ {sum += $13; count++;} END {print "bach avg hrs/week: ", sum/count}' data/adult.data 
+awk -F, 'BEGIN { sum = 0; count = 0} 
+$4 ~ /HS-grad/ {sum += $13; count++;} 
+END {print "hs grad avg hrs/week:", sum/count}' data/adult.data 
+awk -F, 'BEGIN { sum = 0; count = 0} 
+$4 ~ /Bach/ {sum += $13; count++;} 
+END {print "bach avg hrs/week: ", sum/count}' data/adult.data 
